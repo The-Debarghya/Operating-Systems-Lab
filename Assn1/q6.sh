937574bb04e182e9808f33127c4cc403875c2e2c
The-Debarghya <debarghya4444@gmail.com>
2022-08-19 08:56:42 +0530

    Add q7.sh & Update q6.sh

--- Assn1/q6.sh
+++ Assn1/q6.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+echo "Filename to search in:"
+read filename
+echo "Word to search:"
+read word
+
+if [[ grep -Fqw $word $filename ]]; then
+  echo "The number of occurrences of $word:"
+  grep -Eow $word $filename | wc -l
+  echo "Number of occurrences of $word in each line:(count:line no.)"
+  grep -won $word $filename | uniq -c | cut -d ":" -f1 | cut -d " " -f7,8 | tr " " ":"
+else
+  echo "The entered string($word) was not found in $filename"
+fi
---
+++ Assn1/q7.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Filename to search in:"
+read filename
+echo "Word to search:"
+read word1
+echo "Word to replace:"
+read word2
+
+if [[ grep -Fqw $word1 $filename ]]; then
+  sed -i "s/$word1/$word2/g" $filename
+  echo "Replaced all occurrences of $word1 with $word2"
+else
+  echo "The entered string($word1) was not found in $filename"
+fi
