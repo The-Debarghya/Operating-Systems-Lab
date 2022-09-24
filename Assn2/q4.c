69b037e1bdae334fe32d1f7c8b4e8bce042ae434
The-Debarghya <debarghya4444@gmail.com>
2022-09-24 12:53:01 +0530

    Update q4.c

--- Assn2/q4.c
+++ Assn2/q4.c
@@ -3,18 +3,135 @@
 #include <unistd.h>
 #include <time.h>
 #include <semaphore.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/signal.h>
-/*#include <sys/mman.h>
-#include <sys/shm.h>*/
-#define BUFF_LEN 50
-#define PER_PRODUCER_MIN 100
-#define PER_PRODUCER_RANGE 101
+#include <sys/mman.h>
+#define BUFF_LEN 25
+#define PRODUCER_MIN 50
+#define PRODUCER_RANGE 51
 
 typedef struct reqmem{
     int total, start, count;
     unsigned char buffer[BUFF_LEN];
-    sem_t f, e, m;
+    sem_t full, empty, mutex;
 }requiredmemory;
 
+int id, count, total;
+
+void consumer_handler(int sig){
+    printf("Consumer %d : consumed %d items which adds up to: %d\n", id+1, count, total);
+	fflush(stdout);
+	raise(SIGKILL);
+}
+
+int main(int argc, char** argv){
+    total = 0;
+    if (argc != 3){
+		printf("%s: Invalid Arguments\nUsage: %s <producer_count> <consumer_count>\n", argv[0], argv[0]);
+		exit(1);
+    }
+    int P, C;
+	P = atoi(argv[1]);
+	C = atoi(argv[2]);
+    requiredmemory *shared = mmap(NULL, sizeof(shared), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (shared == MAP_FAILED){
+        printf("Memory map failed");
+        exit(1);
+    }
+    shared->count = 0;
+	shared->start = 0;
+	shared->total = 0;
+	sem_t *full, *empty, *mutex;
+	empty = &(shared->empty);
+	full = &(shared->full);
+	mutex = &(shared->mutex);
+    sem_init(empty, 1, BUFF_LEN);
+    sem_init(full, 1, 0);
+    sem_init(mutex, 1, 1);
+    pid_t producers[P], consumers[C];
+    int p, c;
+    for (c = 0; c < C; c++)   {
+        pid_t pid = fork();
+        if (pid < 0)  {
+            goto __fork_error_handler_c;
+        }else if (pid != 0) {
+            consumers[c] = pid;
+        }else {
+            id = c;
+            goto __consumer;
+        }
+        
+    }
+    for (p = 0; p < P; p++){
+        pid_t pid = fork();
+        if (pid < 0)  {
+            goto __fork_error_handler_p;
+        }else if (pid != 0) {
+            producers[p] = pid;
+        }else {
+            id = p;
+            goto __producer;
+        }
+    }
+    while (p--)
+		wait(NULL);
+	while (shared->count)
+		sleep(1);
+	signal(SIGQUIT, SIG_IGN);
+	while (c--){
+		kill(consumers[c], SIGQUIT);
+	}
+	sleep(1);
+	printf("Total : %d\n", shared->total);
+	exit(0);
+    
+    __fork_error_handler_c: {
+        perror("Unexpected error occurred while forking consumers\n");
+	    for (--c; c >= 0; c--)
+		    kill(consumers[c], SIGKILL);
+	    exit(1);
+    }
+    __fork_error_handler_p: {
+        perror("Unexpected error occurred while forking producers\n");
+        for (--c; c >= 0; c--)
+		    kill(consumers[c], SIGKILL);
+	    for (--p; p >= 0; p--)
+		    kill(producers[p], SIGKILL);
+	    exit(1);
+    }
+    __producer: {
+        srand(clock());
+        count = PRODUCER_MIN + rand() % PRODUCER_RANGE;
+        int cnt = count;
+	    while (cnt--){
+		    unsigned char num = 1 + rand() % 80;
+		    sem_wait(empty);
+		    sem_wait(mutex);
+		    shared->buffer[(shared->start + (shared->count)++) % BUFF_LEN] = num;
+		    sem_post(mutex);
+		    sem_post(full);
+		    total += num;
+	    }
+	    printf("Producer %d : produced %d items which adds up to %d\n", id+1, count, total);
+	    return 0;
+    }
+    __consumer: {
+        signal(SIGQUIT, consumer_handler);
+	    count = 0;
+	    while (1){
+		    unsigned char num = 0;
+		    sem_wait(full);
+		    sem_wait(mutex);
+		    num = shared->buffer[shared->start];
+		    shared->start = (shared->start + 1) % BUFF_LEN;
+		    shared->count -= 1;
+		    shared->total += num;
+		    sem_post(mutex);
+		    sem_post(empty);
+		    count++;
+		    total += num;
+	    }
+	    return 0;
+    }
+    
+}
